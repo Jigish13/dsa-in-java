b148c956dbabd180c1fb0af4b4a447f71c00274d
Jigish13 <vyas.jigish13@gmail.com>
2021-12-31 16:02:37 -0500

    Updated ArrayE3.java

--- ArrayE3.java
+++ ArrayE3.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class ArrayE3 {
+import java.util.Arrays;
+
+public class ArrayE3 {
+    public static int[] runningSum(int[] nums) {
+        for (int i = 0, s = 0, l = nums.length; i < l; ++i) {
+            nums[i] += s;
+            s = nums[i];
+        }
+        return nums;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(""+ Arrays.toString(runningSum(nums)));
+    }
 }
